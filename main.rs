58ba99116c186397bc645bb7047c74037a16be0e
carlos97gr <carlosgperu97@gmail.com>
2022-03-14 00:51:55 +0300

    video and audio support

--- main.rs
+++ main.rs
@@ -11,8 +11,10 @@ fn main() {
         dotenv::from_path("./.env").expect("error loading env");
         
         let path_to_images = env::var("PATH_IMAGES").expect("env error");
-        let path_to_installers = env::var("PATH_INSTALLERS").expect("env error");
+        let path_to_videos = env::var("PATH_VIDEOS").expect("env error");
+        let path_to_audios = env::var("PATH_AUDIOS").expect("env error");
         let path_to_documents =env::var("PATH_DOCUMENTS").expect("env error");
+        let path_to_installers = env::var("PATH_INSTALLERS").expect("env error");
 
         for entry in fs::read_dir(env::var("FOLDER_TO_ORGANIZE").expect("env error")).unwrap() {
             let entry = entry.unwrap();
@@ -26,8 +28,7 @@ fn main() {
                     let file_type= Path::new(&path).extension().unwrap();
 
                     //IMAGES
-                    if file_type == "jpeg" ||file_type == "png" || file_type == "jpg" {
-                        
+                    if file_type == "jpeg" ||file_type == "png" || file_type == "jpg" || file_type == "heic" {
                         let destination_file = format!("{}/{}",path_to_images,file_name);
                         fs::copy(path_string,destination_file).expect("copy error");
                         
@@ -37,9 +38,19 @@ fn main() {
                         let destination_file = format!("{}/{}",path_to_installers,file_name);
                         fs::copy(path_string,destination_file).expect("copy error");
                     }
-
-                    if file_type == "pdf" {
+                    //DOCUMENTS
+                    if file_type == "pdf" || file_type == "doc" || file_type == "docx" || file_type == "ppt" || file_type == "xls"{
                         let destination_file = format!("{}/{}",path_to_documents,file_name);
+                        fs::copy(path_string,destination_file).expect("copy error");
+                    }
+                    //VIDEOS
+                    if file_type == "mp4" || file_type == "avi" {
+                        let destination_file = format!("{}/{}",path_to_videos,file_name);
+                        fs::copy(path_string,destination_file).expect("copy error");
+                    }
+                    //AUDIOS
+                    if file_type == "mp3" {
+                        let destination_file = format!("{}/{}",path_to_audios,file_name);
                         fs::copy(path_string,destination_file).expect("copy error");
                     }
 
